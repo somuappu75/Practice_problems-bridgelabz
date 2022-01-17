7c9b2a87d5a6c5c033eaf4da9c9d96e3f8fe1d42
Lenovo <somusp75@gmail.com>
2022-01-17 14:16:12 +0530

    uc2 branch

---
+++ Practice_problems/sting1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_problems
+{
+   public  class sting1
+    {
+        public void uc2()
+        {
+            string str2 = "welcome to uc2";
+            Console.WriteLine(str2);
+        }
+        
+    }
+}
